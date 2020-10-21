30aac9dd9aff646f96ab8d916eef6f44074c38d4
dongchao <3291240985@qq.com>
2020-10-21 14:30:31 +0800

    无修改

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 //12345
-void bubbleSort(vector<int> &num){
+void bubbleSort(vector<int>& num) {
 	int len = num.size();
 
 	for (int i = 0; i < len - 1; i++) {
 		for (int j = 0; j < len - 1 - i; j++) {
 			if (num[j] > num[j + 1])
-			swap(num[j], num[j + 1]);
+				swap(num[j], num[j + 1]);
 		}
-		printf("µÚ%dÂÖ",(i + 1));
+		printf("µÚ%dÂÖ", (i + 1));
 		for (vector<int>::iterator it = num.begin(); it != num.end(); it++) {
-			cout << *it<<" ";
+			cout << *it << " ";
 		}
 		cout << endl;
 
@@ -25,12 +25,10 @@ void bubbleSort(vector<int> &num){
 //second test
 
 
-
-
 int main()
 {
 	vector<int> v;
-	for (int i = 5; i>0; i--)
+	for (int i = 5; i > 0; i--)
 	{
 		v.push_back(i);
 	}
