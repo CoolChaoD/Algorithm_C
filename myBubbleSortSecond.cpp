b7c057a71c18c4209872d5397ccb288c8cd5a828
dongchao <3291240985@qq.com>
2020-10-27 22:44:47 +0800

    找出了代码的错误，其实原因是将循环中的j写成i了

--- myBubbleSortSecond.cpp
+++ myBubbleSortSecond.cpp
@@ -8,15 +8,22 @@ using namespace std;
 
 void BubbleSort(vector<int>& arr) {
 
-	//arr的长度
+	int flag = 0;
 
 	for (int i = 0; i < arr.size() - 1; i++) {
-		for (int j = 0; i < arr.size() - i - 1; j++) {
+		for (int j = 0; j < arr.size() - i - 1; j++) {
 
 			if (arr[j] > arr[j + 1]) {
+				flag = 1;
 				swap(arr[j], arr[j + 1]);
 			}
+		}
 
+		if (flag == 0) { //说明没有发生交换，即已经是按照顺序排好了的
+			break;
+		}
+		else {
+			flag = 0;
 		}
 
 		cout << i + 1 << ":  ";
@@ -32,6 +39,7 @@ void BubbleSort(vector<int>& arr) {
 int main() {
 
 	vector<int> arr = { 1,4,-5,8,9,-10,3 };
+
 
 	BubbleSort(arr);
 
