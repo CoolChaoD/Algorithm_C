a646abddc29046e3df04b9fceb60ea9f96e23f7d
dongchao <3291240985@qq.com>
2020-10-30 08:47:58 +0800

    第五次练习冒泡排序，测试数据写错了，导致结果不正确

---
+++ 5_BubbleSort.cpp
@@ -0,0 +1,40 @@
+/*
+  练习冒泡排序
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+void BubbleSort(vector<int>& arr) {
+	int flag = 0;
+	for (int i = 0; i < arr.size() - 1; i++) {
+		for (int j = 0; j < arr.size() - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				flag = 1;
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+		if (flag == 0) { //说明没有发生交换
+			break;
+		}
+		else {
+			flag = 0;
+		}
+
+	}
+}
+
+
+int main() {
+
+	vector<int> arr = { 2,1,3,4,-1,-5,8,7 };
+	BubbleSort(arr);
+	for (vector<int>::iterator it = arr.begin(); it != arr.end(); it++) {
+		cout << *it << " ";
+	}
+	return 1;
+
+}
