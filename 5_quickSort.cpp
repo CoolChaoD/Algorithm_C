d63d0a770b142e91f4146c71bcc6add5bfcb22cf
dongchao <3291240985@qq.com>
2020-10-30 09:04:58 +0800

    第五次练习快速排序，记住递归在循环外面

--- 5_quickSort.cpp
+++ 5_quickSort.cpp
@@ -1,20 +1,25 @@
+/*
+ 第五次练习快速排序
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-
-
 void quickSort(int left, int right, vector<int>& arr) {
+	//递归的返回条件
 	if (left >= right) {
 		return;
 	}
+
 	int i, j, base;
 	i = left;
 	j = right;
-	base = arr[left];
+	base = arr[left];//选取最左边的数作为基准数
+
 	while (i < j) {
+		//基准数在左边因此先从右边开始
 		while (i < j && arr[j] >= base) {
 			j--;
 		}
@@ -25,7 +30,6 @@ void quickSort(int left, int right, vector<int>& arr) {
 			swap(arr[i], arr[j]);
 		}
 
-
 	}
 	//基数归位
 	arr[left] = arr[i];
@@ -34,7 +38,6 @@ void quickSort(int left, int right, vector<int>& arr) {
 	quickSort(left, i - 1, arr);
 	//向右递归
 	quickSort(j + 1, right, arr);
-
 }
 
 int main() {
@@ -44,6 +47,8 @@ int main() {
 	for (vector<int>::iterator it = arr.begin(); it != arr.end(); it++) {
 		cout << *it << " ";
 	}
+
 	return 1;
 }
+
 
